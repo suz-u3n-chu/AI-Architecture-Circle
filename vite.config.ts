563907f1e82b9ce306ef860dd6af1e595643ad2f
suz-u3n-chu <suz.u3n.chu@gmail.com>
2025-12-10 12:29:02 +0900

    fix: Correct script path for GitHub Pages
    The script path in `index.html` was incorrectly set to `./index.tsx`. This commit changes it to `/index.tsx` to ensure correct loading when hosted on GitHub Pages or other subdirectories. The `base` configuration in `vite.config.ts` was also removed as it's no longer needed with the updated script path.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pagesなどのサブディレクトリでホスティングする場合に必須の設定
-  base: './', 
 });
